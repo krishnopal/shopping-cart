7ee1e52ee3541680173c494801e809044d5612c2
shuvo pal <spshuvo850@gmail.com>
2021-09-07 07:57:46 +0600

    frist commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="">
+        <h1 className='text-primary'>Shuvo</h1>
+    </div>
+  );
+}
+
+export default App;
